b2e94272aeaa21cb92e76464db26512ec3c005a9
Dani Colell <altadais@gmail.com>
2023-06-15 11:38:06 +0200

    Adaptable para mobil

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,13 @@
-const registroButton = document.getElementById("registro");
-const modalForm = document.getElementById("modalForm");
-const closeButton = document.querySelector(".close-button");
+document.addEventListener("DOMContentLoaded", function() {
+  const modal = document.querySelector(".modal");
+  const closeButton = document.querySelector(".cerrar");
+  const registroButton = document.querySelector("#registro");
 
-registroButton.addEventListener("click", () => {
-  modalForm.style.display = "block";
-});
+  registroButton.addEventListener("click", function() {
+    modal.style.display = "block";
+  });
 
-closeButton.addEventListener("click", () => {
-  modalForm.style.display = "none";
+  closeButton.addEventListener("click", function() {
+    modal.style.display = "none";
+  });
 });
